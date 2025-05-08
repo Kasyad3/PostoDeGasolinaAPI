126baa8206ff1a02f06a720ecd499dfd5e057495
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2025-05-07 21:37:27 -0300

    Teste commit pelo git bash

--- obj/Debug/net8.0/PostoDeGasolinaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/PostoDeGasolinaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostoDeGasolinaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02aeb55532cdae747a5f9467311a976224849097")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostoDeGasolinaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostoDeGasolinaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
